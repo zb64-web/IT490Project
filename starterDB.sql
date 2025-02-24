f7cac6fded698d924300493c924dc2c0a0751302
Zubaidah <72946117+zb64-web@users.noreply.github.com>
2025-02-24 17:28:17 +0000

    Update starterDB.sql

--- starterDB.sql
+++ starterDB.sql
@@ -4,7 +4,7 @@ USE starterdb;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (username, password_hash) VALUES ('Ally', PASSWORD('password1'));
+INSERT INTO users (username, password) VALUES ('user1', 'password1');
